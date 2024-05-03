b8ac1a24c37599d32bcaa31cad3f187a3efc6abf
Xennis <code@xennis.org>
2024-05-03 21:28:57 +0200

    Update Cal.com link

--- src/components/calcom.tsx
+++ src/components/calcom.tsx
@@ -14,7 +14,7 @@ export default function CalComIframe() {
   }, [])
   return (
     <Cal
-      calLink="fabian.rosenthal/volunteer-software-support"
+      calLink="fabian.rosenthal/voluntary-support"
       style={{ width: "100%", height: "100%", overflow: "scroll" }}
       config={{ layout: "month_view" }}
     />
