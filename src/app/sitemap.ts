b80d2f0c649930d1f8a2e4c8d390202cb532aa32
Xennis <code@xennis.org>
2024-05-05 22:58:26 +0200

    Set last motified date in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,22 +10,27 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       ...[
         {
           url: `https://${host}${homePage(lang)}`,
+          lastModified: new Date(),
           priority: 1,
         },
         {
           url: `https://${host}${aboutPage(lang)}`,
+          lastModified: new Date(),
           priority: 0.8,
         },
         {
           url: `https://${host}${newsletterPage(lang)}`,
+          lastModified: new Date(),
           priority: 0.8,
         },
         {
           url: `https://${host}${voluntarySupport(lang)}`,
+          lastModified: new Date(),
           priority: 0.7,
         },
         {
           url: `https://${host}${legalPage(lang)}`,
+          lastModified: new Date(),
           priority: 0.1,
         },
       ],
