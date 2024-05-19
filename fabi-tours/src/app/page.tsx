ad8d8c7fa7d31f8bcdc1fa656900d5a7da2bcaef
Xennis <code@xennis.org>
2024-05-19 18:15:44 +0200

    [ft] Fix metadata in layouts

--- fabi-tours/src/app/page.tsx
+++ fabi-tours/src/app/page.tsx
@@ -1,6 +1,16 @@
 import { redirect } from "next/navigation"
+import type { Metadata } from "next"
 
 import { i18n } from "@/content/i18n"
+import { homePage } from "@/content/config"
+import { getMetadata } from "@/content/metadata"
+
+export const metadata: Metadata = {
+  ...getMetadata(i18n.defaultLocale),
+  alternates: {
+    canonical: homePage(i18n.defaultLocale),
+  },
+}
 
 export default function HomePage() {
   redirect(`/${i18n.defaultLocale}`)
--- fabi-tours/src/content/metadata.ts
+++ fabi-tours/src/content/metadata.ts
@@ -1,15 +1,13 @@
 import { i18n } from "@/content/i18n"
-import { host } from "@/content/config"
+import { pageTitle } from "@/content/config"
 
-export const getMetadata = (lang?: string) => {
+export const getMetadata = (lang: string) => {
   const dictionary =
-    lang === undefined || lang === i18n.defaultLocale
+    lang === i18n.defaultLocale
       ? {
-          title: "FabiTours",
           description: "Discover the green spaces of Hamburg",
         }
       : {
-          title: "FabiTours",
           description: "Entdecke die grünen Ecken von Hamburg",
         }
 
@@ -17,24 +15,9 @@ export const getMetadata = (lang?: string) => {
     description: dictionary.description,
     openGraph: {
       description: dictionary.description,
-      title: {
-        default: dictionary.title,
-        template: `%s - ${dictionary.title}`,
-      },
+      // title: Is included as template in the layout
       type: "website",
-      siteName: dictionary.title,
-    },
-    metadataBase: new URL(`https://${host}`),
-    robots: {
-      index: true,
-      follow: true,
-    },
-    title: {
-      default: dictionary.title,
-      template: `%s - ${dictionary.title}`,
-    },
-    verification: {
-      google: process.env.GOOGLE_SITE_VERIFICATION,
+      siteName: pageTitle,
     },
   }
 }
