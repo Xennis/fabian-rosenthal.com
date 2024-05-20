67b2c01eb9d174e535a328f1b17551201aaa7341
Xennis <code@xennis.org>
2024-05-20 17:00:45 +0200

    [ft] Add maskable icons

--- fabi-tours/src/app/manifest.ts
+++ fabi-tours/src/app/manifest.ts
@@ -13,14 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: brandColor,
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: "/icon-384x384.png",
-        sizes: "384x384",
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
       },
     ],
   }
