36d4b5e3545109fd48d36727423733a7e1fc0902
Xennis <code@xennis.org>
2024-05-28 00:10:07 +0200

    [fr] Reduce global CSS

--- fabian-rosenthal/src/components/layout/hero.tsx
+++ fabian-rosenthal/src/components/layout/hero.tsx
@@ -2,7 +2,7 @@ export const Hero = ({ headline, children }: { headline: string; children: React
   return (
     <div className="rounded-xl bg-gradient-to-tl from-blue-700 via-blue-500 to-blue-700 py-28">
       <div className="mx-auto max-w-xl px-4 text-center sm:px-6 lg:px-8">
-        <h2 className="text-3xl tracking-tight text-white sm:text-4xl">{headline}</h2>
+        <h2 className="pb-5 text-3xl font-semibold tracking-tight text-white sm:pb-6 sm:text-4xl">{headline}</h2>
         {children}
       </div>
     </div>
