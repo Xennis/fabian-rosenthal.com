62ddfe906d0c8a1a3ee070c48112dd9c2a12ef69
Xennis <code@xennis.org>
2024-05-27 13:54:27 +0200

    [fr] Resolve links within guide section

--- fabian-rosenthal/src/app/[lang]/guides/business-ideas/[slug]/page.tsx
+++ fabian-rosenthal/src/app/[lang]/guides/business-ideas/[slug]/page.tsx
@@ -1,7 +1,8 @@
-import { getCachedBusinessIdeasPage, getCachedBusinessIdeasPages, getCachedPageContent } from "@/lib/cms/fetchers"
+import { getCachedBusinessIdeasPages, getCachedPageContent } from "@/lib/cms/fetchers"
 import { notFound } from "next/navigation"
 import { Headline } from "@/components/layout/headline"
 import { Render } from "@react-notion-cms/render"
+import { i18n } from "@/content/i18n"
 
 export async function generateStaticParams({ params }: { params: { lang: string } }) {
   // TODO: Remove here + add sitemap
@@ -14,17 +15,32 @@ export async function generateStaticParams({ params }: { params: { lang: string 
 }
 
 export default async function SlugPage({ params }: { params: { lang: string; slug: string } }) {
-  const page = await getCachedBusinessIdeasPage(params)
+  if (params.lang !== i18n.defaultLocale) {
+    notFound()
+  }
+  const pages = await getCachedBusinessIdeasPages()
+  const page = pages.find((p) => p.slug === params.slug) ?? null
   if (page === null) {
     notFound()
   }
   const content = await getCachedPageContent(page.blockId)
 
+  const resolveLinkFn = (nId: string) => {
+    const resolvedPage = pages.find((p) => p.blockId === nId) ?? null
+    if (resolvedPage === null) {
+      return null
+    }
+    return {
+      href: resolvedPage.canonical,
+      icon: resolvedPage.icon,
+    }
+  }
+
   return (
     <div>
       <Headline>{page.title}</Headline>
       <div className="max-width-regular">
-        <Render blocks={content} options={{ formatDateFn: (date) => date.toString(), resolveLinkFn: (nId) => nId }} />
+        <Render blocks={content} options={{ formatDateFn: (date) => date.toString(), resolveLinkFn: resolveLinkFn }} />
       </div>
     </div>
   )
