45f104e7c74a80be603843b1a0bec3e6b7e042e4
Xennis <code@xennis.org>
2024-06-03 11:38:52 +0200

    [fr] Allow calling the host form client-side too

--- fabian-rosenthal/src/content/config.ts
+++ fabian-rosenthal/src/content/config.ts
@@ -4,14 +4,14 @@
 // const headersList = headers()
 // const host = headersList.get("host")!
 const getHost = () => {
-  if (process.env.VERCEL_ENV === "production") {
-    const url = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+    const url = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     if (url) {
       return url
     }
   }
-  if (process.env.VERCEL_ENV === "preview") {
-    const url = process.env.VERCEL_BRANCH_URL
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+    const url = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
     if (url) {
       return url
     }
