b890eb7cb76efdb2023cc2acb1ed10d4f08ad90a
Xennis <code@xennis.org>
2024-06-11 01:43:40 +0200

    [fr] Switch to Vercel blob for CMS images

--- fabian-rosenthal/src/lib/cms/image.ts
+++ fabian-rosenthal/src/lib/cms/image.ts
@@ -1,37 +1,40 @@
-import path from "node:path"
-import { statSync, writeFileSync } from "node:fs"
-
-import nextConfig from "../../../next.config.mjs"
-
-const publicDir = "public"
+import { BlobNotFoundError, head, put } from "@vercel/blob"
 
 const downloadImage = async (url: string, filename: string, lastEditedTime: Date) => {
-  let savedLastEditedTime: Date | null = null
   try {
-    const stat = statSync(filename)
-    savedLastEditedTime = stat.mtime
+    const meta = await head([process.env.BLOB_WORKAROUND_BASE_URL, filename].join("/"))
+    // Avoid download the file again and again
+    if (meta.uploadedAt >= lastEditedTime) {
+      return meta.url
+    }
   } catch (error) {
-    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
+    if (error instanceof BlobNotFoundError) {
       console.debug(`${filename} not found`)
     } else {
       console.warn(`${filename}: ${error}`)
     }
   }
-  // Avoid download the file again and again
-  if (savedLastEditedTime === null || lastEditedTime > savedLastEditedTime) {
-    const resp = await fetch(url)
-    const blob = await resp.blob()
-    writeFileSync(filename, new DataView(await blob.arrayBuffer()))
-    console.debug(`downloaded image ${filename}`)
-  }
+
+  const fetchResp = await fetch(url)
+  const blob = await fetchResp.blob()
+
+  const putResp = await put(filename, blob, {
+    access: "public",
+    addRandomSuffix: false,
+  })
+  console.debug(`downloaded image ${filename}`)
+  return putResp.url
 }
 
 export const downloadImageToPublicDir = async (url: string, filename: string, lastEdited: Date) => {
   const fileUrl = new URL(url)
   const originalFilename = fileUrl.pathname.substring(fileUrl.pathname.lastIndexOf("/") + 1)
   const originalExtension = originalFilename.split(".").pop()
-  const targetFilename = path.join(publicDir, "cms", `${filename}.${originalExtension}`)
+  const targetFilename = [
+    process.env.NEXT_PUBLIC_VERCEL_ENV ?? "preview",
+    "cms",
+    `${filename}.${originalExtension}`,
+  ].join("/")
 
-  await downloadImage(url, targetFilename, lastEdited)
-  return targetFilename.replace(publicDir, nextConfig.basePath ?? "")
+  return await downloadImage(url, targetFilename, lastEdited)
 }
