f8a656d5153fcabffa972b66f7b1d4e061623c38
Xennis <code@xennis.org>
2024-06-10 15:59:28 +0200

    [fr] Update blog layout (mainly adding og image to text)

--- fabian-rosenthal/src/components/layout/headline.tsx
+++ fabian-rosenthal/src/components/layout/headline.tsx
@@ -3,11 +3,23 @@ import { Dot } from "@/components/dot"
 export function Headline({ subtitle, children }: { subtitle?: string; children: React.ReactNode }) {
   return (
     <div className="mb-6 text-center">
-      <h1 className="text-3xl font-semibold leading-tight tracking-tight sm:text-4xl md:text-5xl">
+      <h1 className="text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl">
         {children}
         <Dot />
       </h1>
-      {subtitle && <div className="pt-1.5">{subtitle}</div>}
+      {subtitle && <div className="pt-1.5 text-lg leading-8 tracking-tight text-slate-600">{subtitle}</div>}
     </div>
+  )
+}
+
+export function HeadlineBlog({ subtitle, children }: { subtitle?: string; children: React.ReactNode }) {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl">
+        {children}
+        <Dot />
+      </h1>
+      {subtitle && <div className="pt-2.5 text-lg leading-8 tracking-tight text-slate-600 sm:text-xl">{subtitle}</div>}
+    </>
   )
 }
