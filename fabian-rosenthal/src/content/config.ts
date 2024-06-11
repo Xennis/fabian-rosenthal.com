167b95a5ed79e824f7127dd1cbe335e7785f91e6
Xennis <code@xennis.org>
2024-06-11 08:27:40 +0200

    [fr] Add draft mode endpoints

--- fabian-rosenthal/src/content/config.ts
+++ fabian-rosenthal/src/content/config.ts
@@ -1,3 +1,4 @@
+export const langPage = (lang: string, slug: string) => `/${lang}/${slug}`
 export const aboutPage = (lang: string) => `/${lang}/about`
 export const blogPage = (lang: string) => `/${lang}/blog`
 export const blogPagePost = (lang: string, slug: string) => `/${lang}/blog/${slug}`
@@ -8,6 +9,7 @@ export const homePage = (lang: string) => `/${lang}`
 export const legalPage = (lang: string) => `/${lang}/legal`
 export const newsletterPage = (lang: string) => `/${lang}/newsletter`
 export const voluntarySupport = (lang: string) => `/${lang}/voluntary-support`
+export const apiDisableDraft = `/api/cms/disable-draft`
 
 export const brandColor = "#18b83d"
 
