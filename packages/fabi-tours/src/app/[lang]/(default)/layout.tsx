99c28327efad5a65abdc575658776529e65ffe3d
Xennis <code@xennis.org>
2024-11-23 13:56:43 +0100

    [ft] Remove code (was moved to another repo)

--- packages/fabi-tours/src/app/[lang]/(default)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function DefaultLayout({ children, params }: { children: React.ReactNode; params: { lang: string } }) {
-  return <main className="mx-auto w-full max-w-screen-xl px-3 py-10">{children}</main>
-}
