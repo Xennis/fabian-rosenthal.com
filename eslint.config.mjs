49cb19d86d97adfa1eedc3567a40aa418193c864
Fabian Rosenthal <frosenthal@cc.systems>
2025-06-25 10:51:42 +0200

    chore: Add eslint-plugin-jsx-a11y plugin and address issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import path from "node:path"
-import { fileURLToPath } from "node:url"
-import js from "@eslint/js"
+import jsxA11y from "eslint-plugin-jsx-a11y"
 import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-export default [...compat.extends("next/core-web-vitals")]
+const compat = new FlatCompat()
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    plugins: { "jsx-a11y": jsxA11y },
+  },
+  ...compat.extends("plugin:jsx-a11y/recommended"),
+]
