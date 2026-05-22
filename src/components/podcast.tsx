2f8644918cfc155723e22e703b60d3e23c29e900
Xennis <code@xennis.org>
2026-05-22 20:51:36 +0200

    feat: Feature podcast

---
+++ src/components/podcast.tsx
@@ -0,0 +1,82 @@
+import NextImage from "next/image"
+import { type PodcastSeries, type WithContext } from "schema-dts"
+
+import spotifyIcon from "@/content/images/podcast/spotify.svg"
+import applePodcastsIcon from "@/content/images/podcast/apple-podcasts.svg"
+import rssIcon from "@/content/images/podcast/rss.svg"
+import podcastCover from "@/content/images/podcast/logo-512x512.png"
+
+import { Headline2 } from "@/components/layout/headline"
+import { SocialLink } from "@/components/social-links"
+import { host } from "@/lib/next"
+
+const podcast = {
+  name: "Der Feierabend-Commit",
+  description:
+    "Produktivität durch Technik. Über KI, Automatisierung und smarte Tools aus dem Entwickleralltag. Entspannt, ehrlich und direkt aus dem Feierabend. 🍻",
+  coverImage: podcastCover,
+  feedUrl: "https://anchor.fm/s/112e48794/podcast/rss",
+  originalImageUrl:
+    "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_nologo/46019341/46019341-1779459878789-95e060b532ce5.jpg",
+  platforms: [
+    {
+      label: "Spotify",
+      href: "https://open.spotify.com/show/033l9Xf9LtcTxJWx8OvRgG",
+      imageSrc: spotifyIcon,
+    },
+    {
+      label: "Apple Podcasts",
+      href: "https://podcasts.apple.com/de/podcast/der-feierabend-commit/id1896796759",
+      imageSrc: applePodcastsIcon,
+    },
+    {
+      label: "RSS-Feed",
+      href: "https://anchor.fm/s/112e48794/podcast/rss",
+      imageSrc: rssIcon,
+    },
+  ],
+}
+
+const jsonLd: WithContext<PodcastSeries> = {
+  "@context": "https://schema.org",
+  "@type": "PodcastSeries",
+  name: podcast.name,
+  description: podcast.description,
+  image: podcast.originalImageUrl,
+  url: `https://${host}`,
+  inLanguage: "de",
+  author: { "@type": "Person", name: "Fabian Rosenthal" },
+  webFeed: podcast.feedUrl,
+  sameAs: podcast.platforms.map((p) => p.href),
+}
+
+export function Podcast() {
+  return (
+    <section className="pt-10 md:pt-12">
+      <Headline2>Podcast: {podcast.name}</Headline2>
+      <div className="pt-2 sm:flex sm:gap-6">
+        <NextImage
+          src={podcast.coverImage}
+          alt={`Cover von ${podcast.name}`}
+          width={140}
+          height={140}
+          className="rounded-lg shadow-md"
+        />
+        <div className="pt-4 sm:pt-0">
+          <p className="leading-7">{podcast.description}</p>
+          <ul
+            aria-label={`Plattformen, auf denen ${podcast.name} verfügbar ist`}
+            className="flex items-center gap-4 pt-4"
+          >
+            {podcast.platforms.map((p, i) => (
+              <li key={i}>
+                <SocialLink {...p} width={32} className="duration-500 group-hover:scale-110" />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+    </section>
+  )
+}
---
+++ src/content/images/podcast/README.md
@@ -0,0 +1,7 @@
+# Podcast
+
+## Attribution
+
+- apple-podcasts.svg: [Apple Inc., Public domain, via Wikimedia Commons](<https://commons.wikimedia.org/wiki/File:Podcasts_(iOS).svg>)
+- rss.svg: Feed-icon.svg: unnamed (Mozilla Foundation)User:Jahoe (GPL <http://www.gnu.org/licenses/gpl.html>, LGPL <http://www.gnu.org/licenses/lgpl.html>, MPL 1.1 <https://www.mozilla.org/MPL/1.1/>, MPL 1.1 <https://www.mozilla.org/MPL/1.1/>, GPL <http://www.gnu.org/licenses/gpl.html> or LGPL <http://www.gnu.org/licenses/lgpl.html>), via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Generic_Feed-icon.svg)
+- spotify.svg: [Font Awesome, Spotify, Public domain, via Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Spotify_icon.svg)
